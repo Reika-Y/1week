1053fe7432e89f8178439fc3e1ada3de63b1e78e
Reika-Y <zero.hana1@gmail.com>
2018-11-06 11:15:48 +0900

    cardの実装

--- Project1/Project1/Card.cpp
+++ Project1/Project1/Card.cpp
@@ -10,14 +10,18 @@ const VECTOR2 cardSize = { 60,90 };
 
 /*
 * ｺﾝｽﾄﾗｸﾀ
+* @param (VECTOR2 pos) 座標
+* @param (VECTOR2 offSet)　描画時のｵﾌｾｯﾄ座標
 * @param (HUNDLE hundle) ﾄﾗﾝﾌﾟの柄
 * @param (int num) ﾄﾗﾝﾌﾟの数字
 */
-Card::Card(HUNDLE hundle, int num)
+Card::Card(VECTOR2 pos, VECTOR2 offSet, HUNDLE hundle, int num)
 {
 	ImageMng::GetInstance().GetID(fileName,cardSize,cardDiv);
 	cardInfo.hundle = hundle;
 	cardInfo.num	= num;
+	Card::pos       = pos;
+	Card::offSet    = offSet;
 }
 
 
@@ -28,13 +32,32 @@ Card::~Card()
 // 描画関数
 void Card::Draw(void)
 {
-	DrawGraph(pos + offSet, /*画像のﾊﾝﾄﾞﾙ*/0);
+	int id = 0;
+	DrawGraph(pos + offSet, fileName[id]);
 }
 
 // 更新関数
 bool Card::Update(void)
 {
+}
 
+/*
+*　座標書き換え
+* @param (VECTOR2 pos) 書き換えたい座標
+*/
+bool Card::SetPos(VECTOR2 pos)
+{
+	Card::pos = pos;
+	return true;
+}
+/*
+* ｵﾌｾｯﾄ座標書き換え
+* @param (VECTOR2 offSet)	書き換えたいｵﾌｾｯﾄ座標
+*/
+bool Card::SetOffSet(VECTOR2 offSet)
+{
+	Card::offSet = offSet;
+	return true;
 }
 
 int DrawGraph(VECTOR2 pos, int GrHundle)
--- Project1/Project1/Card.h
+++ Project1/Project1/Card.h
@@ -22,10 +22,12 @@ CardInfo cardInfo;
 class Card
 {
 public:
-	Card(HUNDLE hundle,int num);
+	Card(VECTOR2 pos, VECTOR2 offSet, HUNDLE hundle,int num);
 	~Card();
 	void Draw(void);		// 描画関数
 	bool Update(void);		// 更新関数
+	bool SetPos(VECTOR2 pos);				// 座標書き換え
+	bool SetOffSet(VECTOR2 offSet); 		// ｵﾌｾｯﾄ座標書き換え
 private:
 	VECTOR2 pos;		// 座標
 	VECTOR2 offSet;		// ｵﾌｾｯﾄ座標
