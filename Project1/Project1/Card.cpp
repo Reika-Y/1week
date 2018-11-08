9a2cf80495b0052f35a15daf66c0362f30899868
Reika-Y <zero.hana1@gmail.com>
2018-11-08 10:47:28 +0900

    確認

--- Project1/Project1/Card.cpp
+++ Project1/Project1/Card.cpp
@@ -1,5 +1,4 @@
 #include "DxLib.h"
-#include "VECTOR2.h"
 #include "Card.h"
 #include "ImageMng.h"
 
@@ -7,11 +6,6 @@
 const string fileName = "image/trump.png";
 const VECTOR2 cardDiv = { 8,8 };
 const VECTOR2 cardSize = { 60,90 };
-
-Card::Card()
-{
-	ImageMng::GetInstance().GetID(fileName, cardSize, cardDiv);
-}
 
 /*
 * ∫›Ωƒ◊∏¿
@@ -38,7 +32,7 @@ Card::~Card()
 void Card::Draw(void)
 {
 	int id = 0;
-	DrawGraph(pos + offSet, fileName[id]);
+	DrawGraph(pos + offSet, fileName[0]);
 }
 
 // çXêVä÷êî
