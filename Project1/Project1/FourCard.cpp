ac0e4ede12835e36ce20e0f6d1cbf0c9bd0265ed
omomuro <omomuro@kk.kk>
2018-11-09 12:53:18 +0900

    判定処理を軽く書いたのを上げておく

--- Project1/Project1/FourCard.cpp
+++ Project1/Project1/FourCard.cpp
@@ -20,3 +20,17 @@ int FourCard::GetPoint(void)
 {
 	return 6;
 }
+
+//bool FourCard::RoleJuge(/*HUNDLE hd1, int num1, HUNDLE hd2, int num2*/)
+//{
+//	if (num1 == num2)
+//	{
+//		cnt++;
+//	}
+//	if (cnt == 3)
+//	{
+//		return true;
+//	}
+//	return false;
+//}
+
